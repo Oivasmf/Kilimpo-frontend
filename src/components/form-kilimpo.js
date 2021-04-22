028c2d3dc3cc76aa4bdb7c394589285b51b7269c
Ubuntu <ubuntu@ip-172-31-39-145.ec2.internal>
2021-04-22 23:29:52 +0000

    Committer: Ubuntu <ubuntu@ip-172-31-39-145.ec2.internal>
     On branch master
    
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   README.md
    	new file:   package-lock.json
    	new file:   package.json
    	new file:   public/favicon.ico
    	new file:   public/index.html
    	new file:   public/manifest.json
    	new file:   public/robots.txt
    	new file:   src/App.css
    	new file:   src/App.js
    	new file:   src/App.test.js
    	new file:   src/components/form-kilimpo.js
    	new file:   src/index.css
    	new file:   src/index.js
    	new file:   src/logo.svg
    	new file:   src/reportWebVitals.js
    	new file:   src/setupTests.js
    	new file:   yarn-error.log
    	new file:   yarn.lock

---
+++ src/components/form-kilimpo.js
@@ -0,0 +1,240 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class FormKilimpo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.backendUrl = 'http://ec2-54-175-209-214.compute-1.amazonaws.com:3389/kilimpo';
+
+        this.baseState = {
+            nome:'',
+            telefone:'',
+            whatsapp:'',
+            marca:'',
+            modelo:'',
+            ano:'',
+            horario:'',
+            placa:'',
+            whats: [
+                { value:'Sim', label: "Sim" },
+                { value:'Nao', label: "Não" },
+            ],
+            data:'',
+            horarios: [
+                { value: '08:00h', label: "08:00h" },
+                { value: '08:30h', label: "08:30h" },
+                { value: '09:00h', label: "09:00h" },
+                { value: '09:30h', label: "09:30h" },
+                { value: '10:00h', label: "10:00h" },
+                { value: '10:30h', label: "10:30h" },
+                { value: '11:00h', label: "11:00h" },
+                { value: '11:30h', label: "11:30h" },
+                { value: '12:00h', label: "12:00h" },
+                { value: '12:30h', label: "12:30h" },
+                { value: '13:00h', label: "13:00h" },
+                { value: '13:30h', label: "13:30h" },
+                { value: '14:00h', label: "14:00h" },
+                { value: '14:30h', label: "14:30h" },
+                { value: '15:00h', label: "15:00h" },
+                { value: '15:30h', label: "15:30h" },
+                { value: '16:00h', label: "16:00h" },
+                { value: '16:30h', label: "16:30h" },
+                { value: '17:00h', label: "17:00h" },
+                { value: '17:30h', label: "17:30h" },
+            ],
+            contexto: {}
+        }// fim de this.baseState
+
+        this.state = this.baseState;
+
+        this.onChangeNome = this.onChangeNome.bind(this);
+        this.onChangeTelefone = this.onChangeTelefone.bind(this);
+        this.onChangeWhatsapp = this.onChangeWhatsapp.bind(this);
+        this.onChangeMarca = this.onChangeMarca.bind(this);
+        this.onChangeModelo = this.onChangeModelo.bind(this);
+        this.onChangeAno = this.onChangeAno.bind(this);
+        this.onChangePlaca = this.onChangePlaca.bind(this);
+        this.onChangeData = this.onChangeData.bind(this);
+        this.onChangeHorario = this.onChangeHorario.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onReset = this.onReset.bind(this);
+    }// fim do constructor()
+
+    onChangeNome(e) {
+        this.setState({ nome: e.target.value })
+    }
+    onChangeTelefone(e) {
+        this.setState({ telefone: e.target.value })
+    }
+    onChangeWhatsapp(e) {
+        this.setState({ whatsapp: e.target.value })
+    }
+    onChangeMarca(e) {
+        this.setState({ marca: e.target.value })
+    }
+    onChangeModelo(e) {
+        this.setState({ modelo: e.target.value })
+    }
+    onChangeAno(e) {
+        this.setState({ ano: e.target.value })
+    }
+    onChangePlaca(e) {
+        this.setState({ placa: e.target.value })
+    }
+    onChangeData(e) {
+        this.setState({ data: e.target.value })
+    }
+    onChangeHorario(e) {
+        this.setState({ horario: e.target.value })
+    }
+
+    onReset(e) {
+        this.setState(this.baseState);
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const kilimpo = {
+            nome:this.state.nome,
+            telefone:this.state.telefone,
+            whatsapp:this.state.whatsapp,
+            marca:this.state.marca,
+            modelo:this.state.modelo,
+            ano:this.state.ano,
+            placa:this.state.placa,
+            horario:this.state.horario,
+            data:this.state.data,
+        };// fim do const kilimpo
+
+        axios.post(this.backendUrl, kilimpo)
+            .then(res => this.setState({ contexto: res.data }))
+            .catch(erro => this.setState({ contexto: erro.response.data }));
+            
+        this.setState(this.baseState);
+    }// fim do onSubmit()
+
+    render() {
+        const contexto = this.state.contexto;
+        let erros = [];
+        if (contexto.erros) {
+            erros = contexto.erros.map(
+                (erro, idx) => (
+                <li key={idx}>{erro.msg}</li>));
+            }
+            let kilimpo = [];
+            if(contexto.kilimpo) {
+                kilimpo = [
+                    (<li key='1'>
+                        <b>Nome:</b> {contexto.kilimpo.nome}
+                    </li>),
+                    (<li key='2'>
+                        <b>Telefone:</b> {contexto.kilimpo.telefone}
+                    </li>),
+                    (<li key='3'>
+                        <b>Whatsapp:</b> {contexto.kilimpo.whatsapp}
+                    </li>),
+                    (<li key='4'>
+                        <b>Marca do veículo:</b> {contexto.kilimpo.marca}
+                    </li>),
+                    (<li key='5'>
+                        <b>Modelo:</b> {contexto.kilimpo.modelo}
+                    </li>),
+                    (<li key='6'>
+                        <b>Ano:</b> {contexto.kilimpo.ano}
+                    </li>),
+                    (<li key='7'>
+                        <b>Placa:</b> {contexto.kilimpo.placa}
+                    </li>),
+                    (<li key='8'>
+                     <b>Data:</b> {contexto.kilimpo.data}
+                    </li>),
+                    (<li key='9'>
+                        <b>Horario:</b> {contexto.kilimpo.horario}
+                    </li>),
+                    ]
+            } // fim do if (contexto.kilimpo)
+            
+            return (
+                <>
+                    <h1>
+                        Lavação automotiva KiLimpo
+                    </h1>
+                    <form onSubmit={this.onSubmit}>
+                        <fieldset>
+                            <legend>Agendamento</legend>
+                            Nome completo: *<br />
+                            <input type="text" value={this.state.nome}
+                                onChange={this.onChangeNome} /><br />
+
+                            Telefone: <br />
+                            <input type="text" value={this.state.telefone}
+                            onChange={this.onChangeTelefone} /><br />
+
+                            Este número possui Whatsapp? * ([NÃO] caso prefira não informar o telefone)
+                            <br />
+                            {
+                                this.state.whats.map((obj, idx) => {
+                                    return(<React.Fragment key={idx}>
+                                        <input type="radio" name="whatsapp"
+                                            checked={this.state.whatsapp === obj.value}
+                                            value={obj.value}
+                                            onChange={this.onChangeWhatsapp} />
+                                        {obj.label}
+                                    </React.Fragment>);
+                                })
+                            }
+                            <br />
+                            <br />
+                            <br />
+                            Marca do veículo: *<br />
+                            <input type="text" value={this.state.marca}
+                                onChange={this.onChangeMarca} /><br />
+
+                            Modelo: *<br />
+                            <input type="text" value={this.state.modelo}
+                                onChange={this.onChangeModelo} /><br />
+
+                            Ano: <br />
+                            <input type="text" value={this.state.ano}
+                                onChange={this.onChangeAno} /><br />
+
+                            Placa: *<br />
+                            <input type="text" value={this.state.placa}
+                                onChange={this.onChangePlaca} /><br /><br /><br />
+
+                            Data e horário de atendimento *<br />
+                            <input type="date" value={this.state.data}
+                                onChange={this.onChangeData}/>
+
+
+
+
+                            <select value={this.state.horario}
+                                onChange={this.onChangeHorario}>
+                                <option value="-1">--Selecione o horário de atendimento</option>
+                                {
+                                    this.state.horarios.map(obj => {
+                                        return(<option key={obj.value}
+                                            value={obj.value}>{obj.label}</option>);
+                                    })
+                                }
+                            </select><br />
+                            
+                            <hr />
+                            <input type="submit" value="Enviar" />
+                            <input type="button" value="Limpar"
+                                onClick={this.onReset} />
+                            * Campos obrigatórios
+                        </fieldset>
+                    </form>
+                    {
+                        contexto.erros && <ul>{erros}</ul>
+                    }
+                    
+                    <h2>Dados recebidos:</h2>
+                    {contexto.kilimpo && <ul>{kilimpo}</ul>}
+                </>
+            );// fim do return
+        }// fim do render()
+    }// fim da classe Formkilimpo

f97772b7fc18097ffa8730d6d8c78a6ca9f58d93
Fernando Jacques Peiretti <fernandopeiretti@users.noreply.github.com>
2021-04-24 19:45:27 -0300

    Update form-kilimpo.js

--- src/components/form-kilimpo.js
+++ src/components/form-kilimpo.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default class FormKilimpo extends Component {
     constructor(props) {

060431c2c3d8221f2740d3894a0e2f32f19e7001
Fernando Jacques Peiretti <fernandopeiretti@users.noreply.github.com>
2021-04-24 19:57:08 -0300

    Update form-kilimpo.js

--- src/components/form-kilimpo.js
+++ src/components/form-kilimpo.js
@@ -5,6 +5,7 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class FormKilimpo extends Component {
     constructor(props) {

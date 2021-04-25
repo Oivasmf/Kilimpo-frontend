d964004f7029b27db885da85d5443f08184787ed
Fernando Jacques Peiretti <fernandopeiretti@users.noreply.github.com>
2021-04-25 20:12:39 -0300

    Update form-kilimpo.js

--- src/components/form-kilimpo.js
+++ src/components/form-kilimpo.js
@@ -125,8 +125,8 @@ export default class FormKilimpo extends Component {
         const contexto = this.state.contexto;
         let erros = [];
         if (contexto.erros) {
-            console.log(erros); 
-            console.log(contexto.erros); 
+            console.log(erros.length); 
+            console.log(contexto.erros.length); 
             console.log(erros === contexto.erros);
             if (erros === contexto.erros){
             }
